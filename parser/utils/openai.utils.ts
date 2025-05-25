d9039df26a29188b8f46d650c22850cb541b5d0e
Ben Noyman <B.noyman@outlook.com>
2025-05-25 19:35:14 +0300

    update openai

--- parser/utils/openai.utils.ts
+++ parser/utils/openai.utils.ts
@@ -1,8 +1,6 @@
 import { OpenAI } from "openai";
 import { CATEGORIES } from "../constants/categories";
-
-//const GOOGLE_GEOCODING_API_KEY = process.env.GOOGLE_API_KEY;
-//const GOOGLE_GEOCODING_BASE_URL = process.env.GOOGLE_GEOCODING_BASE_URL;
+import { storePrompt } from "../constants/prompts";
 
 const openai = new OpenAI({ apiKey: process.env.OPENAI_API_KEY });
 
@@ -14,8 +12,8 @@ export async function fixStoreData(data: {
   chainname: string;
   subchainname: string;
 }) {
-  const prompt = `אתה מקבל אובייקט JSON המכיל מידע על חנות אך הערכים שלו מבולגנים בין השדות. תסדר את הערכים של השדות הבאים בלבד: "storename", "address", "city", "zipcode". התוצאה צריכה להיות JSON בלבד בלי מילים נוספות. הנה האובייקט:
-${JSON.stringify(data, null, 2)}`;
+  const prompt =
+    storePrompt + `\nהנה האובייקט:\n${JSON.stringify(data, null, 2)}`;
 
   const chatCompletion = await openai.chat.completions.create({
     model: "gpt-4.1-mini",
@@ -66,30 +64,3 @@ async () => {
   const data = await fixStoreData(object);
   console.log(data);
 };
-
-// export async function fetchCoordinates(
-//   query: string
-// ): Promise<{ lat: number; lon: number } | null> {
-//   const url = `${GOOGLE_GEOCODING_BASE_URL}?address=${encodeURIComponent(
-//     query
-//   )}&key=${GOOGLE_GEOCODING_API_KEY}&language=iw`;
-
-//   try {
-//     const res = await fetch(url);
-//     const data = await res.json();
-
-//     if (data.status !== "OK" || !data.results.length) {
-//       console.warn(`Google Geocoding failed for "${query}": ${data.status}`);
-//       return null;
-//     }
-
-//     const result = data.results[0];
-//     const { geometry } = result;
-//     const { lat, lon } = geometry.location;
-
-//     return { lat, lon };
-//   } catch (err) {
-//     console.error(`Google Geocoding error for "${query}":`, err);
-//     return null;
-//   }
-// }
