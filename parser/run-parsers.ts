f6b78b3a0e0f0d5c26f7de1d8f06a8f7a4b2eb2e
ShlomiHaser <hasshlomi21@gmail.com>
2025-05-26 18:51:26 +0300

    added running codes scripts

--- parser/run-parsers.ts
+++ parser/run-parsers.ts
@@ -8,43 +8,30 @@ import { processAllStoresFiles } from "./modules/stores/stores.service.js";
 
 const filename = fileURLToPath(import.meta.url);
 const dirname = path.dirname(filename);
+const args = process.argv.slice(2);
 
 async function run() {
   try {
     await prisma.$queryRaw`SELECT 1`;
+    const actions = new Set(args);
+    if (actions.has("stores")) {
+      const storesPath = path.join(dirname, "..", "scraper-engine", "output", "stores");
+      console.log("🏬 Processing stores...");
+      await processAllStoresFiles(storesPath);
+    }
 
-    // Process stores
-    // const storesPath = path.join(
-    //   dirname,
-    //   "..",
-    //   "scraper-engine",
-    //   "output",
-    //   "stores"
-    // );
-    // console.log("🚀 Processing stores...");
-    // await processAllStoresFiles(storesPath);
+    if (actions.has("groceries")) {
+      const groceriesPath = path.join(dirname, "..", "scraper-engine", "output", "groceries");
+      console.log("🥬 Processing groceries...");
+      await processAllGroceriesFiles(groceriesPath);
+    }
 
-    // // Process groceries
-    const groceriesPath = path.join(
-      dirname,
-      "..",
-      "scraper-engine",
-      "output",
-      "groceries"
-    );
-    console.log("🥬 Processing groceries...");
-    await processAllGroceriesFiles(groceriesPath);
+    if (actions.has("promotions")) {
+      const promotionsPath = path.join(dirname, "..", "scraper-engine", "output", "promotions");
+      console.log("💸 Processing promotions...");
+      await processAllPromotionsFiles(promotionsPath);
+    }
 
-    // Process promotions
-    // const promotionsPath = path.join(
-    //   dirname,
-    //   "..",
-    //   "scraper-engine",
-    //   "output",
-    //   "promotions"
-    // );
-    // console.log("💸 Processing promotions...");
-    // await processAllPromotionsFiles(promotionsPath);
 
     console.log("✅ All data processed successfully.");
   } catch (error) {
