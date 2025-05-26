b4e946b78df813af23a5bb5c577b48b6ef0d9a92
ShlomiHaser <hasshlomi21@gmail.com>
2025-05-26 19:26:36 +0300

    fixed bugs

--- parser/run-parsers.ts
+++ parser/run-parsers.ts
@@ -14,23 +14,24 @@ async function run() {
   try {
     await prisma.$queryRaw`SELECT 1`;
     const actions = new Set(args);
-    if (actions.has("stores")) {
+
+   if (actions.has("stores")) {
       const storesPath = path.join(dirname, "..", "scraper-engine", "output", "stores");
       console.log("🏬 Processing stores...");
       await processAllStoresFiles(storesPath);
-    }
+   }
 
-    if (actions.has("groceries")) {
+   if (actions.has("groceries")) {
       const groceriesPath = path.join(dirname, "..", "scraper-engine", "output", "groceries");
       console.log("🥬 Processing groceries...");
       await processAllGroceriesFiles(groceriesPath);
-    }
+   }
 
     if (actions.has("promotions")) {
       const promotionsPath = path.join(dirname, "..", "scraper-engine", "output", "promotions");
       console.log("💸 Processing promotions...");
       await processAllPromotionsFiles(promotionsPath);
-    }
+   }
 
 
     console.log("✅ All data processed successfully.");
